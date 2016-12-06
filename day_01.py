6015d45b1d71c4a2477062009bf59c1325927e6f
Dan Manastireanu <danmana11@yahoo.com>
2016-12-06 02:04:40 +0200

    Day 1, part 2

--- day_01.py
+++ day_01.py
@@ -2,15 +2,13 @@ from util import maybe_download
 
 dirs = 'NESWN'
 
-
-def next_pos(x, y, dir, op):
+def next_dir(dir, op):
     if op[0] == 'R':
-        dir = dirs[dirs.index(dir) + 1]
+        return dirs[dirs.index(dir) + 1]
     else:
-        dir = dirs[dirs.rindex(dir) - 1]
+        return dirs[dirs.rindex(dir) - 1]
 
-    n = int(op[1:])
-
+def next_pos(x,y, dir, n):
     if dir == 'N':
         y -= n
     elif dir == 'E':
@@ -19,6 +17,16 @@ def next_pos(x, y, dir, op):
         y += n
     else:
         x -= n
+    return (x,y)
+
+
+def fast_forward(x, y, dir, op):
+    dir = next_dir(dir, op)
+
+    n = int(op[1:])
+
+    x,y = next_pos(x, y, dir, n)
+
     return (x, y, dir)
 
 
@@ -28,11 +36,35 @@ def part1():
     dir = 'N'
     with maybe_download(1) as file:
         data = [s.strip() for s in file.read().split(',')]
+
         for op in data:
-            x, y, dir = next_pos(x, y, dir, op)
+            x, y, dir = fast_forward(x, y, dir, op)
+
         dist = abs(x) + abs(y)
         print('part1:', dist)
 
 
+def part2():
+    visited = set()
+    x = 0
+    y = 0
+    dir = 'N'
+    visited.add((x,y))
+    with maybe_download(1) as file:
+        data = [s.strip() for s in file.read().split(',')]
+
+        for op in data:
+            dir = next_dir(dir, op)
+            n = int(op[1:])
+            for i in range(n):
+                x,y = next_pos(x,y,dir,1)
+                if (x,y) in visited:
+                    dist = abs(x) + abs(y)
+                    print('part1:', dist)
+                    return
+                else:
+                    visited.add((x,y))
+
 part1()
+part2()
 
