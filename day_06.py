1a65b720f0611ec0bd87815547b5193159352a96
Dan Manastireanu <danmana11@yahoo.com>
2016-12-11 14:57:04 +0200

    Day 6, part 1

---
+++ day_06.py
@@ -0,0 +1,40 @@
+from util import maybe_download
+
+
+def get_counts(s):
+    count = dict()
+    for ch in s:
+        if ch in count:
+            count[ch] += 1
+        else:
+            count[ch] = 1
+    count = [(ch, count[ch]) for ch in count]
+    count.sort(key=lambda x: x[1], reverse=True)
+    return count
+
+
+def part1():
+    with maybe_download(6) as file:
+        counts = None
+        for s in file:
+            s = s.strip()
+            if counts is None:
+                counts = []
+                for i in range(len(s)):
+                    counts.append(dict())
+            for i, ch in enumerate(s):
+                c = counts[i]
+                if ch in c:
+                    c[ch] += 1
+                else:
+                    c[ch] = 1
+
+        counts = [[(ch, c[ch]) for ch in c] for c in counts]
+        for c in counts:
+            c.sort(key=lambda x: x[1], reverse=True)
+
+        letters = ''.join([c[0][0] for c in counts])
+        print('part1', letters)
+
+
+part1()
