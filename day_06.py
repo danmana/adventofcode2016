5e080ff3cbc37ddb506272c8e055f58f8074d1d7
Dan Manastireanu <danmana11@yahoo.com>
2016-12-12 18:29:08 +0200

    Day 6, part 2

--- day_06.py
+++ day_06.py
@@ -12,29 +12,38 @@ def get_counts(s):
     count.sort(key=lambda x: x[1], reverse=True)
     return count
 
+def get_all_counts(file):
+    counts = None
+    for s in file:
+        s = s.strip()
+        if counts is None:
+            counts = []
+            for i in range(len(s)):
+                counts.append(dict())
+        for i, ch in enumerate(s):
+            c = counts[i]
+            if ch in c:
+                c[ch] += 1
+            else:
+                c[ch] = 1
+    counts = [[(ch, c[ch]) for ch in c] for c in counts]
+    for c in counts:
+        c.sort(key=lambda x: x[1], reverse=True)
+    return counts
+
 
 def part1():
     with maybe_download(6) as file:
-        counts = None
-        for s in file:
-            s = s.strip()
-            if counts is None:
-                counts = []
-                for i in range(len(s)):
-                    counts.append(dict())
-            for i, ch in enumerate(s):
-                c = counts[i]
-                if ch in c:
-                    c[ch] += 1
-                else:
-                    c[ch] = 1
-
-        counts = [[(ch, c[ch]) for ch in c] for c in counts]
-        for c in counts:
-            c.sort(key=lambda x: x[1], reverse=True)
-
+        counts = get_all_counts(file)
         letters = ''.join([c[0][0] for c in counts])
         print('part1', letters)
 
+def part2():
+    with maybe_download(6) as file:
+        counts = get_all_counts(file)
+        letters = ''.join([c[-1][0] for c in counts])
+        print('part2', letters)
+
 
 part1()
+part2()
